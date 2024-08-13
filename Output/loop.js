548e7ea609866dbfa5295aa29fcc4ffa213027c9
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-13 22:28:39 +0600

    added for loop

---
+++ Output/loop.js
@@ -0,0 +1,14 @@
+//......loop.....(ekoi kaj bar bar korte loop use kora hoy)
+
+//    for loop
+
+// for(starting, condition, update) {
+//     console.log()
+// }
+
+for (var x = 1; x <= 10; x = x + 1) {
+    document.write('Bangladesh <br/>')
+}
+for (var x = 10; x >= 1; x = x - 1) {
+    document.write(' ' + x)
+}
