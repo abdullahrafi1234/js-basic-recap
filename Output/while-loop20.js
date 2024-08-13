7e476f36ff26be606a237bc475b5f4b273e069a0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-14 01:15:37 +0600

    added while loop

---
+++ Output/while-loop20.js
@@ -0,0 +1,19 @@
+// for (var i = 1; i< 100; i=i+1){
+//     document.write(" "+i)
+// }
+
+//------ while loop -----(starting surote diye dite hobe and body te update part ta dite hbe,, body te update na dile bar bar cholte thakbe)
+
+var i = 1 ;
+var sum = 0;
+
+
+while(i <= 50) {
+    
+    if(i % 3 == 0 && i%5 == 0){
+        sum = sum+i;
+    }
+    i = i+1
+}
+
+document.write(sum);
