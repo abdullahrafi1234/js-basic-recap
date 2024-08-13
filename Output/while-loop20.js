2c112d5dfeb34c3e2b814fa7f479ca4b10d89db8
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-14 02:09:55 +0600

    added ternary operator

--- Output/while-loop20.js
+++ Output/while-loop20.js
@@ -4,7 +4,7 @@
 
 //------ while loop -----(starting surote diye dite hobe and body te update part ta dite hbe,, body te update na dile bar bar cholte thakbe)
 
-var i = 1 ;
+var i = 1 ; // starting age dite hbe
 var sum = 0;
 
 
@@ -13,7 +13,10 @@ while(i <= 50) {
     if(i % 3 == 0 && i%5 == 0){
         sum = sum+i;
     }
-    i = i+1
+    i = i+1 //body te update condition dite hbe
 }
 
 document.write(sum);
+
+
+//do while loop.......
