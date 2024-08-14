46aa391ba40ea64fc5809dfe8607ccb3e4c85ed8
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-14 15:21:35 +0600

    added array loop

--- Output/array.js
+++ Output/array.js
@@ -22,3 +22,21 @@ var country2 = ['sl', 'pak', 'nzl']
 
 var country = country1.concat(country2)
 console.log(country)
+
+
+//-------array loop-------
+
+var num = [10, 20, 30, 40, 50, 60]
+
+for (var i = 0; i < num.length; i++) {
+    console.log(num[i])
+}
+
+//------ for loop er moode sum ----------
+var nums = [10, 20, 30, 40, 50, 60]
+var sum = 0;
+
+for (var i = 0; i < nums.length; i++) {
+    sum = sum + nums[i]
+}
+console.log(sum)
